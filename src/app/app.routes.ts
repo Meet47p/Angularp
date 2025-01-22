9b40e5a2fe69ab40cf8d371c8a824e3bba011e41
Meet47p <meetponkiya4@gmail.com>
2025-01-22 17:36:52 +0530

    delete all unused component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { UserComponent } from '../Component/user/user.component';
-import { AdminComponent } from '../Component/admin/admin.component';
+
 import { HomeComponent } from '../Component/home/home.component';
 import { LoginComponent } from '../Component/login/login.component';
 import { SignupComponent } from '../Component/signup/signup.component';
